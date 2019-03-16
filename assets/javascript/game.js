e07cf224091393758a6e7e19a618c5dfca752df3
JacobBraxley <jacob@roboticsys.com>
2019-03-16 02:30:18 -0400

    Async Issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,54 @@
 
 var hangManGame = {
     //Variables
-    winCount: 0,
-    lossCount: 0, //It really depends how you define a loss.  You are acting within the constraints we've defined.
-    numGuessesLeft: 0b1010, //10 guesses.  I don't want people to lose really.  Just accept the inevitable. 
-    currentTargetIndex: 0,
-    wordsToGuess: ["Skynet", "Daleks", "Borg", "Bender", "Ultron"],
+    currentTargetIndex: -1,     
+    activeWord: document.getElementById("activeWord"),
+    wordsToGuess: ["SKYNET", "DALEKS", "BORG", "BENDER", "ULTRON"],
     wordInProgressSounds: ["skynet.mp3", "dalek.wav", "borg.mp3", "bender.mp3", "ultron.mp3"],
     wordWinSounds: ["skynetWin.wav", "dalekWin.mp3", "borgWin.mp3", "benderWin.mp3", "benderWin.mp3"],
     userGuesses: [],
-    revealedWord: "",
-    activeWord: document.getElementById("activeWord"),
-    guessesLeftDisplay: document.getElementById("remainingGuesses"),
+    numGuessesLeft: 0b1010, //10 guesses.  I don't want people to lose really.  Just accept the inevitable. 
+    audio: undefined,
+    verboseLog: true,
 
     //Functions
+    sleep: function(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    },
+
+    pauseFor: async function(ms) {
+        if(this.verboseLog) { console.log("Before Sleep"); }
+        await this.sleep(ms);
+        if(this.verboseLog) { console.log("After Sleep"); } 
+    },
+
+    handleAudioPromise: function (promise, count)  {
+        if(promise !== undefined) {
+            promise.then(_ => {
+                //It should be playing.
+            })
+            .catch(error => {
+                //Something went wrong.
+                if(count < 2) { //Max Attempts
+                    //Try again in half a second.
+                    hangManGame.pauseFor(500);
+                    hangManGame.handleAudioPromise(hangManGame.audio.play(), ++count);
+                }
+                else {
+                    console.log("Could not get it play in 5 attempts.");
+                }
+                //try again
+            });
+        }
+    },
+
     playAudioFile: function (audioFile, loopIt = true) {
-        var audio = document.getElementById("myAudio");
-        audio = new Audio('assets/audio/' + audioFile);
-        audio.loop = loopIt;
-        audio.play();
+        if(this.audio != undefined)
+            this.audio.pause();
+        this.audio = new Audio('assets/audio/' + audioFile);
+        this.audio.autoplay = true;
+        this.audio.loop = loopIt;
+        this.handleAudioPromise(this.audio.play(), 0);
     },
 
     //Create a new array (mapped) off the old array using transform.
@@ -30,39 +60,33 @@ var hangManGame = {
         return mapped;
     },
 
-    testMe: function (testVar) {
-        var search = "bob";
-
-        var result;
-        if (this.userGuesses.includes("x")) {
-            result = [String("x").toUpperCase(), " "];
-        } else {
-            result = ["_", " "];
-        }
-        console.log(result);
-        console.log(this.userGuesses.includes(testVar) ? [String(testVar).toUpperCase(), " "] : ["_", " "]);
-    },
-
+    //Primary User Update function.
     progressDisplay: function () {
-        this.guessesLeftDisplay.innerHTML = this.numGuessesLeft.toString(2);
-        var currentTarget = this.wordsToGuess[this.currentTargetIndex];
+        //Guesses Left
+        document.getElementById("remainingGuesses").innerHTML = this.numGuessesLeft.toString(2);
 
+        //Active Word
+        var currentTarget = this.wordsToGuess[this.currentTargetIndex];
         //foreach char in the array, add it and a space if you can find it in the user guesses, otherwise add _ and a space.
         var asArray = this.map(currentTarget, x => (this.userGuesses.includes(x) ? String(x).toUpperCase() : "_"));
 
         var asStringWithSpaces = asArray.join(' ');
-
-        //update the website
         this.activeWord.innerHTML = asStringWithSpaces;
+
+        //Leters guessed
+        document.getElementById("guessesSoFar").innerHTML = this.userGuesses.join(' '); 
+      
         return asStringWithSpaces;
     },
 
+    //Transition function.
     loadNextWord: function () {
-        //Update Index, sound, guesses avaialble, clear guess array, display word.
-        this.wordInProgressSounds(++this.currentTargetIndex);
+        //Update Index, sound, guesses avaialble, clear guess array, display word.        
+        this.currentTargetIndex++;
         this.numGuessesLeft = 0b1010;
-        this.userGuesses = [];
-        this.progressDisplay();
+        this.userGuesses = [];        
+        this.playAudioFile(this.wordInProgressSounds[this.currentTargetIndex]);
+        hangManGame.progressDisplay();
     },
 
     evaluateWinLoss: function () {
@@ -70,33 +94,30 @@ var hangManGame = {
             var as = "bob";
         }
         else if (!this.activeWord.innerHTML.includes("_")) { //Win
-            this.playAudioFile(this.wordWinSounds[this.currentTargetIndex], false);
-            window.setTimeout(this.loadNextWord, 3000);
+            //debugger;
+            hangManGame.playAudioFile(hangManGame.wordWinSounds[hangManGame.currentTargetIndex], false);
+            this.pauseFor(8000);
+            this.loadNextWord();
             //TODO add show function for progress.
         }
     },
 
     handleKeyPress: function (key) {
-        var myRegEx = /[^a-z]/i;
-        if (!myRegEx.test(key)) {
-            var toUpper = String(key).toUpperCase();
+        //Regex for any single Alpha Character Case Insensitive.
+        var myRegEx = /\b[a-z]\b/i;
+        if (myRegEx.test(key)) {
+            var asUpper = String(key).toUpperCase();
             //Ignore repicks.
-            if (!this.userGuesses.includes(toUpper)) {
-                //Your robot overlords are happy to count non-alpha keys as wrong guesses.
-                //petition if you think our compassion subroutines are at all likely to activate.
-                this.userGuesses.push(toUpper);
-                if (this.wordWinSounds[this.currentTargetIndex].includes(toUpper)) {
-                    this.progressDisplay();
-                }
-                else {
+            if (!this.userGuesses.includes(asUpper)) {
+                this.userGuesses.push(asUpper);
+
+                if (!this.wordsToGuess[this.currentTargetIndex].includes(asUpper)) {
                     this.numGuessesLeft--;
                 }
 
-                document.getElementById("guessesSoFar").innerHTML += " " + toUpper;
+                this.progressDisplay();
+                this.evaluateWinLoss();
             }
-
-            this.guessesLeftDisplay.innerHTML = this.numGuessesLeft.toString(2);
-            this.evaluateWinLoss();
         }
     }
 };
@@ -105,5 +126,9 @@ document.onkeyup = function (event) {
     hangManGame.handleKeyPress(event.key);
 }
 
-//Initial Display.
-hangManGame.progressDisplay();
+hangManGame.loadNextWord();
+
+//Initial Display.  Javascript Document.Ready
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     window.setTimeout(function () { hangManGame.loadNextWord(); }, 1000);    
+// });
